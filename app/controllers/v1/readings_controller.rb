0c194b795bb53aaa5b8f5494d28f008b79090636
danthedrummer <dan.downey.contact@gmail.com>
2018-05-01 12:29:38 +0000

    Refactoring reading index
    I had an idea to clean up the reading index method because it was a mess

--- app/controllers/v1/readings_controller.rb
+++ app/controllers/v1/readings_controller.rb
@@ -6,26 +6,25 @@ class V1::ReadingsController < ApplicationController
       return
     end
     
-    @readings = []
     if params.has_key?(:sensor)
-      current_user.device.reports.each do |report|
-        report.readings.each do |reading|
-          if reading.sensor.shortname == params[:sensor]
-            @readings << {:sensor => reading.sensor.shortname, :value => reading.value, 
-              :time_reported => reading.report.time_reported}
-          end
-        end
-      end
-    else
-      current_user.device.reports.each do |report|
-        report.readings.each do |reading|
-          @readings << {:sensor => reading.sensor.shortname, :value => reading.value, 
-            :time_reported => reading.report.time_reported}
-        end
+      sensor = Sensor.find_by_shortname(params[:sensor])
+    end
+    
+    retrieved_readings = []
+    current_user.device.reports.each do |report|
+      if params.has_key?(:sensor)
+        retrieved_readings.concat(report.readings.where(sensor: sensor))
+      else
+        retrieved_readings.concat(report.readings)
       end
     end
-    result = {:readings => @readings}
     
-    render json: result, status: :ok
+    result = []
+    retrieved_readings.each do |reading|
+      result << {:sensor => sensor.shortname, :value => reading.value, 
+        :time_reported => reading.report.time_reported}
+    end
+    
+    render json: {:readings => result}, status: :ok
   end
 end
